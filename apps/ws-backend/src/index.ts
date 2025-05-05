8234917cb110b9d12cf94fb5ae2c573ddc145734
yashkatore31 <ybkatore31@gmail.com>
2025-05-05 12:02:53 +0530

    created a frotend and backend

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,12 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3080 });
+
+wss.on('connection', function connection(ws) {
+
+    ws.on('message', function message(data) {
+    ws.send('pong');
+  });
+
+
+});
